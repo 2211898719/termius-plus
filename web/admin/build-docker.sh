3bfe98e79378539bc203386f6c14cd723c5d98b5
hongjunlong <2211898719@qq.com>
2024-04-27 20:25:05 +0800

    fix: 添加应用和监控

--- web/admin/build-docker.sh
+++ web/admin/build-docker.sh
@@ -15,10 +15,10 @@ fi
 
 yarn build
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/education-portal/termius-plus:front-$1 .
+docker build -t hongjunlong/termius-plus:front-$1 .
 
-docker login registry.cn-hangzhou.aliyuncs.com
+docker login hongjunlong/termius-plus
 
-docker push registry.cn-hangzhou.aliyuncs.com/education-portal/termius-plus:front-$1
+docker push hongjunlong/termius-plus:front-$1
 
 
